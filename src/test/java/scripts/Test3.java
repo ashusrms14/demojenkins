30583100d13ba4494a7b53bee1de7e1f0d7da908
SAN <SAN@192.168.1.37>
2022-09-12 19:19:16 +0530

    Framework

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 
+
+{
+  @Test
+  public void tc2()
+  {
+	  Reporter.log("hello pythonselenium",true);
+  }
+}
